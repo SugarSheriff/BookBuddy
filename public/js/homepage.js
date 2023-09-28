b1840344921a1428a7702e7d10e1976923feed97
Javier <elchinatalpf@github.com>
2023-09-27 19:36:04 -0500

    Added api logic in booksRoutes and homepage, added userRoutes.js

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -30,61 +30,62 @@ const renderTemplate = async () => {
   document.body.innerHTML = renderedHtml;
 };
 
-const fetchBooks = async (searchTerm) => {
-  try {
+// const fetchBooks = async (searchTerm) => {
+//   try {
 
-    const apiKey = 'AIzaSyDClhvWRLjTV_3Ivco7Wq3tsDt8-yh38rc';
-    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=${apiKey}&maxResults=3`);
-    const result = await response.json();
-    console.log(result);
-    const books = result.items.slice(0, 3).map(item => ({
-      title: item.volumeInfo.title,
-      author: item.volumeInfo.authors?.join(', '),
-      description: item.volumeInfo.description,
-        thumbnail: item.volumeInfo.imageLinks.thumbnail
-    }));
-    // data.featuredBooks = books;
-  } catch (err) {
-    console.error("Error fetching books:", err);
-  }
-};
+//     const apiKey = 'AIzaSyDClhvWRLjTV_3Ivco7Wq3tsDt8-yh38rc';
+//     const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=${apiKey}&maxResults=3`);
+//     const result = await response.json();
+//     console.log(result);
+//     const books = result.items.slice(0, 3).map(item => ({
+//       title: item.volumeInfo.title,
+//       author: item.volumeInfo.authors?.join(', '),
+//       description: item.volumeInfo.description,
+//         thumbnail: item.volumeInfo.imageLinks.thumbnail
+//     }));
+//     // data.featuredBooks = books;
+//   } catch (err) {
+//     console.error("Error fetching books:", err);
+//   }
+// };
+const displayResults = (books) => {
+  const resultsContainer = document.querySelector('.myBooks');
+  resultsContainer.innerHTML = '';
 
-const displayBooks = (books) => {
-  const booksContainer = document.querySelector('.myBooks');
-  booksContainer.innerHTML = '';
   books.forEach(book => {
-      const bookDiv = document.createElement('div');
-      bookDiv.classList.add('book-box');
-      bookDiv.innerHTML = `
-          <h3 class="title">${book.title}</h3>
-          <img src="${book.thumbnail}" alt="${book.title} cover">
-          <p>${book.author}</p>
-          <p>${book.description}</p>
-      `;
-      booksContainer.appendChild(bookDiv);
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('book-box');
+    bookDiv.innerHTML = `
+      <h3>${book.volumeInfo.title}</h3>
+      <img src="${book.volumeInfo.imageLinks?.thumbnail || 'default-image-url.jpg'}" alt="${book.volumeInfo.title} cover">
+      <p>${book.volumeInfo.authors?.join(', ') || 'Unknown Author'}</p>
+      <p>${book.volumeInfo.description || 'No description available.'}</p>
+    `;
+    resultsContainer.appendChild(bookDiv);
   });
-  console.log(books);
 };
 
 const init = async () => {
-  // await renderTemplate();
   const form = document.querySelector('.box form');
   form.addEventListener('submit', async (event) => {
-      event.preventDefault();
-      const searchTerm = event.target.elements.search.value.trim();
-      if (!searchTerm) return;
-      const response = await fetch('/api/books/search?q=' + searchTerm);
-      console.log("This is the:", response);
-      const books = await response.json();
+    event.preventDefault();
 
-      displayBooks(books);
-      console.log(books)
-      // await fetchBooks(searchTerm);
-      // await renderTemplate();
+    const searchTerm = event.target.elements.search.value.trim();
+    if (!searchTerm) return;
+
+    // Call the `/api/books/search` endpoint to search for books.
+    const response = await fetch('/api/books/search?q=' + searchTerm);
+
+    // Parse the response from the endpoint.
+    const books = await response.json();
+
+    // Display the results.
+    displayResults(books);
   });
 };
 
 init();
+
 
 // response: items.volumInfo.title
 // items.volumInfo.author
